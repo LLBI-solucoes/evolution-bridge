e9c054f0648c9d27c54be8320b06773a43422bb4
Saulo Costa <saulotarsobc@gmail.com>
2025-04-12 14:14:29 -0300

    update interfaces

--- src/lib/EvolutionBridge.ts
+++ src/lib/EvolutionBridge.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { EvolutionBridgeConfig, Post, SendText } from '../types';
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { EvolutionBridgeConfig, Post, SendText } from "../types";
 
 /**
  * Classe para interagir com o EvolutionBridge
@@ -8,56 +8,59 @@ import { EvolutionBridgeConfig, Post, SendText } from '../types';
  * @example
  * const evolutionBridge = new EvolutionBridge({
  *     apiKey: '123456789',
- *     instance: 'my-instance',   
+ *     instance: 'my-instance',
  })
  */
 export class EvolutionBridge {
-    private url: string;
-    private apiKey: string;
-    private instance: string;
+  private client: AxiosInstance;
+  private instance: string;
 
-    private client: AxiosInstance;
-
-    constructor(config: EvolutionBridgeConfig) {
-        if (!config.url) throw new Error('url é obrigatória para inicializar o EvolutionBridge');
-        if (!config.apiKey) throw new Error('apiKey é obrigatória para inicializar o EvolutionBridge');
-        if (!config.instance) throw new Error('instance é obrigatória para inicializar o EvolutionBridge');
-
-        this.client = axios.create();
-
-        this.url = config.url;
-        this.apiKey = config.apiKey;
-        this.instance = config.instance;
+  constructor(config: EvolutionBridgeConfig) {
+    if (!config.url) {
+      throw new Error("url é obrigatória para inicializar o EvolutionBridge");
     }
 
-
-    /**
-     * Envia um texto para um número
-     * @param {SendText} sendText - Objeto com número e texto
-     * @returns {Promise<T>} - Retorna uma promise com o resultado da requisição
-     * @example
-     * const result = await evolutionBridge.sendText({
-     *     number: '123456789',
-     *     text: 'Olá, mundo!',
-     * })
-     */
-    async sendText<T>({ number, text }: SendText): Promise<AxiosResponse<T>> {
-        return this.post({
-            url: `${this.url}/message/sendText/${this.instance}`,
-            data: {
-                number,
-                text
-            },
-            config: {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'apikey': this.apiKey,
-                },
-            },
-        })
+    if (!config.apiKey) {
+      throw new Error(
+        "apiKey é obrigatória para inicializar o EvolutionBridge"
+      );
     }
 
-    private async post<T = any>({ url, data, config }: Post): Promise<AxiosResponse<T>> {
-        return this.client.post<T>(url, data, config);
+    if (!config.instance) {
+      throw new Error(
+        "instance é obrigatória para inicializar o EvolutionBridge"
+      );
     }
+
+    this.client = axios.create({
+      baseURL: config.url,
+      headers: {
+        "Content-Type": "application/json",
+        apikey: config.apiKey,
+      },
+    });
+
+    this.instance = config.instance;
+  }
+
+  /**
+   * Envia um texto para um número
+   * @param {SendText} data - Objeto com número e texto
+   * @returns {Promise<T>} - Retorna uma promise com o resultado da requisição
+   * @example
+   * const result = await evolutionBridge.sendText({
+   *     number: '123456789',
+   *     text: 'Olá, mundo!',
+   * })
+   */
+  async sendText<T>(data: SendText): Promise<AxiosResponse<T>> {
+    return this.post({
+      url: `/message/sendText/${this.instance}`,
+      data,
+    });
+  }
+
+  private async post<T>({ url, data }: Post): Promise<AxiosResponse<T>> {
+    return this.client.post<T>(url, data);
+  }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { AxiosRequestConfig } from "axios";
-
 /**
  * Interface para configuração do EvolutionBridge
  * @interface EvolutionBridgeConfig
@@ -7,9 +5,41 @@ import { AxiosRequestConfig } from "axios";
  * @property {string} instance - Instância do EvolutionBridge
  */
 export interface EvolutionBridgeConfig {
-    url: string;
-    apiKey: string;
-    instance: string;
+  url: string;
+  apiKey: string;
+  instance: string;
+}
+
+/**
+ * Interface para enviar mensagem
+ * @interface SendText
+ * @property {string} number - Número do destinatário
+ * @property {string} text - Texto a ser enviado
+ */
+interface Quoted {
+  key: {
+    id: string;
+  };
+  message: {
+    conversation: string;
+  };
+}
+
+/**
+ * Interface para configuração de opções
+ * @interface Options
+ * @property {number} delay - Tempo de espera em milissegundos
+ * @property {Quoted} quoted - Habilita a citação
+ * @property {boolean} linkPreview - Habilita a previsualização de links
+ * @property {boolean} mentionsEveryOne - Habilita a mencão para todos
+ * @property {string[]} mentioned - Lista de IDs de usuários para mencionar
+ */
+export interface Options {
+  delay?: number;
+  quoted?: Quoted;
+  linkPreview?: boolean;
+  mentionsEveryOne?: boolean;
+  mentioned?: string[];
 }
 
 /**
@@ -18,9 +48,9 @@ export interface EvolutionBridgeConfig {
  * @property {string} number - Número do destinatário
  * @property {string} text - Texto a ser enviado
  */
-export interface SendText {
-    number: string,
-    text: string
+export interface SendText extends Options {
+  number: string;
+  text: string;
 }
 
 /**
@@ -31,7 +61,6 @@ export interface SendText {
  * @property {AxiosRequestConfig} config - Configuração da requisição
  */
 export interface Post {
-    url: string,
-    data?: any,
-    config?: AxiosRequestConfig
+  url: string;
+  data: any;
 }
