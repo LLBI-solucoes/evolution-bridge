b014ea05a29628ce9525b72d6f5ca5365479ac04
Saulo Costa <saulotarsobc@gmail.com>
2025-04-12 14:46:56 -0300

    add JSDoc

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,22 @@
+/**
+ * Interface para configuração do EvolutionBridge
+ * @interface EvolutionBridgeConfig
+ * @description Configuração do EvolutionBridge
+ * @property {string} url - URL da API da Evolution
+ * @property {string} apiKey - Chave de API da Evolution
+ * @property {string} instance - Nome da instância da Evolution
+ */
 export interface EvolutionBridgeConfig {
   url: string;
   apiKey: string;
   instance: string;
 }
 
+/**
+ * Interface para um quoted
+ * @interface quoted
+ * @description Objeto que representa um quoted
+ */
 interface Quoted {
   key: {
     id: string;
@@ -13,6 +26,16 @@ interface Quoted {
   };
 }
 
+/**
+ * Interface para opções
+ * @interface Options
+ * @description Opções para enviar um texto para um número
+ * @property {number} delay - Delay em milissegundos
+ * @property {Quoted} quoted - Objeto que representa um quoted
+ * @property {boolean} linkPreview - Indica se o link deve ser previsto
+ * @property {boolean} mentionsEveryOne - Indica se todos os usuários devem ser mencionados
+ * @property {string[]} mentioned - Lista de usuários a serem mencionados
+ */
 interface Options {
   delay?: number;
   quoted?: Quoted;
@@ -21,11 +44,26 @@ interface Options {
   mentioned?: string[];
 }
 
+/**
+ * Interface para enviar um texto para um número
+ * @interface SendText
+ * @description Objeto para enviar um texto para um número
+ * @extends {Options}
+ * @property {string} number - Número do destinatário
+ * @property {string} text - Texto a ser enviado
+ */
 export interface SendText extends Options {
   number: string;
   text: string;
 }
 
+/**
+ * Interface para uma requisição POST
+ * @interface Post
+ * @description Objeto para uma requisição POST
+ * @property {string} url - URL da requisição
+ * @property {any} data - Dados da requisição
+ */
 export interface Post {
   url: string;
   data: any;
