06f9c29c3f69791fd35731cf2dc6887c9cacd4ec
Saulo Costa <saulotarsobc@gmail.com>
2025-04-12 15:35:23 -0300

    v0.0.32

--- src/lib/EvolutionBridge.ts
+++ src/lib/EvolutionBridge.ts
@@ -45,22 +45,10 @@ export class EvolutionBridge {
     this.instance = config.instance;
   }
 
-  /**
-   * Sends a text message using the EvolutionBridge.
-   * @template T - The expected response type.
-   * @param {SendText} param0 - Object containing the text data and optional configuration.
-   * @param {object} param0.data - Data for the text message.
-   * @param {string} param0.data.number - Recipient's number.
-   * @param {string} param0.data.text - Text to be sent.
-   * @param {number} [param0.data.delay] - Delay in seconds before sending the text.
-   * @param {Quoted} [param0.data.quoted] - Referenced message.
-   * @param {boolean} [param0.data.linkPreview] - Show link preview.
-   * @param {boolean} [param0.data.mentionsEveryOne] - Mention all users in the conversation.
-   * @param {string[]} [param0.data.mentioned] - Numbers of users to be mentioned.
-   * @param {AxiosRequestConfig} [param0.config] - Optional request configuration.
-   * @returns {Promise<AxiosResponse<T>>} - Returns a promise with the response of the request.
-   */
-  async sendText<T>({ data, config }: SendText): Promise<AxiosResponse<T>> {
+  async sendText<T>({
+    data,
+    config = {},
+  }: SendText): Promise<AxiosResponse<T>> {
     return this.post({
       url: `/message/sendText/${this.instance}`,
       data,
@@ -68,19 +56,10 @@ export class EvolutionBridge {
     });
   }
 
-  /**
-   * Sends a POST request to the EvolutionBridge.
-   * @template T - The expected response type.
-   * @param {Post} param0 - Object containing the request data and optional configuration.
-   * @param {string} param0.url - URL of the request.
-   * @param {object} param0.data - Data for the request.
-   * @param { {params?: any} } [param0.config] - Optional request configuration.
-   * @returns {Promise<AxiosResponse<T>>} - Returns a promise with the response of the request.
-   */
   private async post<T>({
     url,
     data,
-    config,
+    config = {},
   }: Post): Promise<AxiosResponse<T>> {
     return this.client.post<T>(url, data, config);
   }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,11 @@
 import { AxiosRequestConfig } from "axios";
 
-/**
- * Interface para configuração do EvolutionBridge
- * @interface EvolutionBridgeConfig
- * @description Configuração do EvolutionBridge
- * @property {string} url - URL da API da Evolution
- * @property {string} apiKey - Chave de API da Evolution
- * @property {string} instance - Nome da instância da Evolution
- */
 export interface EvolutionBridgeConfig {
   url: string;
   apiKey: string;
   instance: string;
 }
 
-/**
- * Interface para um quoted
- * @interface quoted
- * @description Objeto que representa um quoted
- */
 interface Quoted {
   key: {
     id: string;
@@ -28,42 +15,23 @@ interface Quoted {
   };
 }
 
-/**
- * Interface para envio de texto via EvolutionBridge
- * @interface SendText
- * @description Configura o envio de um texto via EvolutionBridge
- * @property {object} data - Dados do texto
- * @property {string} data.number - N mero do destinat rio
- * @property {string} data.text - Texto a ser enviado
- * @property {number} [data.delay] - Tempo em segundos para aguardar antes de enviar o texto
- * @property {Quoted} [data.quoted] - Mensagem referenciada
- * @property {boolean} [data.linkPreview] - Mostrar preview de links
- * @property {boolean} [data.mentionsEveryOne] - Marcar todos os usu rios da conversa
- * @property {string[]} [data.mentioned] - N meros dos usu rios a serem mencionados
- * @property {AxiosRequestConfig} [config] - Configura o da requisi o
- */
+export interface SendTextPayload {
+  number: string;
+  text: string;
+  delay?: number;
+  quoted?: Quoted;
+  linkPreview?: boolean;
+  mentionsEveryOne?: boolean;
+  mentioned?: string[];
+}
+
 export interface SendText {
-  data: {
-    number: string;
-    text: string;
-    delay?: number;
-    quoted?: Quoted;
-    linkPreview?: boolean;
-    mentionsEveryOne?: boolean;
-    mentioned?: string[];
-  };
+  data: SendTextPayload;
   config?: {
-    params?: any;
+    params?: Record<string, any>;
   };
 }
 
-/**
- * Interface para uma requisição POST
- * @interface Post
- * @description Objeto para uma requisição POST
- * @property {string} url - URL da requisição
- * @property {any} data - Dados da requisição
- */
 export interface Post {
   url: string;
   data?: any;
